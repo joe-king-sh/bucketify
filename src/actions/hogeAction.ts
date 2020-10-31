12313d5d4f96629f292a99c334d3f90af952d42e
Joe King <a8109058@gmail.com>
2020-10-31 13:00:57 +0900

    restruction

--- src/actions/hogeAction.ts
+++
@@ -1,8 +0,0 @@
-import actionCreatorFactory from 'typescript-fsa';
-
-const actionCreator = actionCreatorFactory();
-
-export const hogeActions = {
-  updateName: actionCreator<string>('ACTIONS_UPDATE_NAME'),
-  updateEmail: actionCreator<string>('ACTIONS_UPDATE_EMAIL')
-};
