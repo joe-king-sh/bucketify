59cadb52bf93dbfcf9036f0ad10d1f0f76392d39
Joe King <a8109058@gmail.com>
2020-11-03 02:04:55 +0900

    Add generic template page.

---
+++ bucketify/src/components/pages/MyBuckets.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyBuckets: React.FC = () => {
+    return <h1>My Buckets</h1>
+}
+
+export default MyBuckets
---
+++ bucketify/src/components/templates/theme.ts
@@ -0,0 +1,53 @@
+import React from 'react';
+
+// materilai-ui
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+
+
+
+const makeCustomTheme = (isDarkMode: boolean) => {
+
+    const fontFamily = [
+        "Noto Sans JP",
+        "Lato",
+        "游ゴシック Medium",
+        "游ゴシック体",
+        "Yu Gothic Medium",
+        "YuGothic",
+        "ヒラギノ角ゴ ProN",
+        "Hiragino Kaku Gothic ProN",
+        "メイリオ",
+        "Meiryo",
+        "ＭＳ Ｐゴシック",
+        "MS PGothic",
+        "sans-serif",
+    ].join(",");
+
+    let theme = createMuiTheme({
+        typography: {
+            fontFamily: fontFamily,
+        },
+        palette: {
+            primary: {
+            light: '#4c5869',
+            main: '#232f3e',
+            dark: '#000518',
+            contrastText: '#ffffff',
+            },
+            secondary: {
+            light: '#ff9b41',
+            main: '#ff6904',
+            dark: '#c43700',
+            contrastText: '#000000',
+            },
+            type: isDarkMode ? "dark" : "light",
+        },
+    });
+    theme = responsiveFontSizes(theme);
+
+    return theme
+}
+
+
+export default makeCustomTheme ;
