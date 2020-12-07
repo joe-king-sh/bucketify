c8e8d6e6c66520f50faa68788277e75b706ac179
Joe King <a8109058@gmail.com>
2020-12-08 01:00:44 +0900

    Add validation in scanBucket #2

--- bucketify/src/components/99_common/message.ts
+++ bucketify/src/components/99_common/message.ts
@@ -1,3 +1,9 @@
-// Messages tempalte master.
+/* Messages tempalte master. */
 
-export const requiredValueEmpty = ({requiredValue}:{requiredValue: string}) => `Enter ${requiredValue}.`;
+// validation error
+export const msgRequiredValueEmpty = ({requiredValue}:{requiredValue: string}) => `Enter ${requiredValue}.`;
+
+// s3 error message
+export const msgInValidAccessKey = () => `Check your access key is correct.`;
+export const msgSignatureDoesNotMatch = () => `The request signature we calculated does not match the signature you provided. \n Check your secret access key is correct.`;
+export const msgNetworkingError = () => `Check s3 bucket name is exists.`;
