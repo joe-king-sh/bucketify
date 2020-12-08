3b47268577d587cef156bce29e02513b4ead3598
Joe King <a8109058@gmail.com>
2020-12-09 00:15:36 +0900

    Add getObject and Putitem operation in ScanBucket.tsx

--- bucketify/src/components/99_common/message.ts
+++
@@ -1,9 +0,0 @@
-/* Messages tempalte master. */
-
-// validation error
-export const msgRequiredValueEmpty = ({requiredValue}:{requiredValue: string}) => `Enter ${requiredValue}.`;
-
-// s3 error message
-export const msgInValidAccessKey = () => `Check your access key is correct.`;
-export const msgSignatureDoesNotMatch = () => `The request signature we calculated does not match the signature you provided. \n Check your secret access key is correct.`;
-export const msgNetworkingError = () => `Check s3 bucket name is exists.`;
