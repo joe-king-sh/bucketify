5560e629fe760c34f8f6432d07387c6f142491a1
Joe King <a8109058@gmail.com>
2020-12-14 21:50:28 +0900

    Rebuild directory structure.

---
+++ bucketify/src/components/02_organisms/footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+
+import { Author } from '../../common/const'
+
+import clsx from "clsx";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+
+    footer: {
+      backgroundColor: theme.palette.primary.main,
+      minHeight: '2rem',
+    },
+  })
+);
+
+export const Footer: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+
+    <Box className={clsx(classes.footer)}>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"© "}
+        {new Date().getFullYear()}
+        {" Copyright "}
+        <Link color="inherit" to="/">
+          {Author}
+        </Link>
+        {"."}
+      </Typography>
+    </Box>
+  );
+};
